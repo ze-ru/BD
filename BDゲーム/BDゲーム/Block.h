d012b94cf0fccc49eb863c6fd6097b5701b80116
望斗季 寺井 <h1201916002@hiratagakuen.onmicrosoft.com>
2019-10-24 10:47:31 +0900

    map1仮作成

--- BDゲーム/BDゲーム/Block.h
+++ BDゲーム/BDゲーム/Block.h
@@ -4,17 +4,17 @@
 //使用するネームスペース
 using namespace GameL;
 
-//オブジェクト：ブロック＆背景
+//オブジェクト：ブロック
 class CObjBlock :public CObj
 {
 public :
-	CObjBlock() {};
+	CObjBlock(int map[10][157]);
 	~CObjBlock() {};
 	void Init();     //イニシャライズ
 	void Action();   //アクション
 	void Draw();     //ドロー
 private:
-	int m_map[10][20]; //マップ情報（仮）
+	int m_map[10][157]; //マップ情報
 
 	float m_scroll; //左右スクロール用
 };
--- BDゲーム/BDゲーム/SceneStage1.cpp
+++ BDゲーム/BDゲーム/SceneStage1.cpp
@@ -6,6 +6,8 @@
 #include"GameL\SceneObjManager.h"
 #include"GameL\DrawTexture.h"
 
+#include"GameL\UserData.h"
+
 //使用ネームスペース
 using namespace GameL;
 
@@ -28,6 +30,25 @@ CSceneStage1::~CSceneStage1()
 //初期化メソッド
 void CSceneStage1::InitScene()
 {
+	//外部データの読み込み
+	unique_ptr<wchar_t>p;//ステージ情報ポインター
+	int size;//ステージ情報の大きさ
+	p = Save::ExternalDataOpen(L"stage1.csv", &size);//外部データ読み込み
+
+	int map[10][157];
+	int count = 1;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 157; j++)
+		{
+			int w = 0;
+			swscanf_s(&p.get()[count], L"%d", &w);
+
+			map[i][j] = w;
+			count += 2;
+		}
+	}
+
 	//グラフィック読み込み
 	Draw::LoadImageW(L"ORIGIN.png",1,TEX_SIZE_512);
 	Draw::LoadImageW(L"Stage2..png",0, TEX_SIZE_512);
@@ -37,11 +58,15 @@ void CSceneStage1::InitScene()
 	CObjHero*obj = new CObjHero();
 	Objs::InsertObj(obj, OBJ_HERO, 10);
 
-	CObjBlock*objb = new CObjBlock();
+	//Blockオブジェクト作成
+	CObjBlock*objb = new CObjBlock(map);
 	Objs::InsertObj(objb, OBJ_BLOCK, 11);
 
 	CObjStage1*objs1 = new CObjStage1();
 	Objs::InsertObj(objs1, OBJ_STAGE1, 1);
+
+	
+
 }
 
 //実行中メソッド
