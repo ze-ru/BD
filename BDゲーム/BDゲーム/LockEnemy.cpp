92691bba23e8f64435b6629d12844d9ebc8bdc35
岩本紫苑 <h1201916004@hiratagakuen.onmicrosoft.com>
2019-10-24 10:27:43 +0900

    変更

--- BDゲーム/BDゲーム/LockEnemy.cpp
+++ BDゲーム/BDゲーム/LockEnemy.cpp
@@ -13,6 +13,7 @@ void CObjLockEnemy::Init()
 	m_px = 0;
 	m_ani_time = 0;
 	m_ani_frame = 0;
+	m_posture = 0;
 }
 void CObjLockEnemy::Action()
 {
@@ -27,17 +28,17 @@ void CObjLockEnemy::Draw()
 
 	src.m_top = 0.0f;
 	src.m_left = 0.0f;
-	src.m_right = src.m_right+64.0f;
-	src.m_bottom = src.m_top+64.0f;
+	src.m_right =32.0f;
+	src.m_bottom =32.0f;
 
 
 	//
 	dst.m_top = m_py;
-	dst.m_left = (80.0f * m_posture)+m_px;
-	dst.m_right = (80 - 80.0f * m_posture)+64.0f;
-	dst.m_bottom =dst.m_top+64.0f;
+	dst.m_left = m_px;
+	dst.m_right =64.0f;
+	dst.m_bottom =64.0f;
 
 
 	//
-	Draw::Draw(3, &src, &dst, c, 0.0f);
+	Draw::Draw(4, &src, &dst, c, 0.0f);
 }
