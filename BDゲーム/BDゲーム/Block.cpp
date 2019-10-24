b840a4a514a67c801a56e3d9522a2169485f9895
望斗季 寺井 <h1201916002@hiratagakuen.onmicrosoft.com>
2019-10-24 15:52:33 +0900

    y

--- BDゲーム/BDゲーム/Block.cpp
+++ BDゲーム/BDゲーム/Block.cpp
@@ -41,90 +41,6 @@ void CObjBlock::Action()
 		hero->SetX(300);
 		m_scroll -= hero->GetVX();
 	}
-
-	hero->SetUp(false);
-	hero->SetDown(false);
-	hero->SetLeft(false);
-	hero->SetRight(false);
-
-	for (int i = 0; i < 11; i++)
-	{
-		for (int j = 0; j < 157; j++)
-		{
-			//m_mapの全要素にアクセス
-			if (m_map[i][j] > 0)
-			{
-				//要素番号を座標に変更
-				float x = j * 64.0f;
-				float y = i * 64.0f-64.0f;
-
-				//主人公とブロックの当たり判定
-				if ((hx+(-m_scroll)+64.0f>x) && (hx+(-m_scroll)<x+64.0f) && (hy+64.0f>y) && (hy<y+64.0f))
-				{
-					//上下左右判定
-
-					//vectorの作成
-					float vx =(hx+(-m_scroll)) - x;
-					float vy = hy - y;
-
-					//長さを求める
-					float len = sqrt(vx * vx + vy * vy);
-
-					//角度を求める
-					float r = atan2(vy, vx);
-					r = r * 180.0f / 3.14f;
-
-					if (r <= 0.0f)
-						r = abs(r);
-					else
-						r = 360.0f - abs(r);
-				//lenがある一定の長さより短い場合判定に入る
-					if (len < 88.0f)
-					{
-						//角度で上下左右を判定
-					//右
-						if ((r < 45 && r > 0) || r > 315)
-						{
-							hero->SetRight(true);
-							hero->SetX(x + 64.0f+(m_scroll));
-							hero->SetVX(-hero->GetVX()*0.1f);
-						}
-
-						//上
-						if (r > 45 && r < 135)
-						{
-							hero->SetDown(true);
-							hero->SetY(y - 64.0f);
-							hero->SetVY(0.0f);
-						}
-
-						//左
-						if (r > 135 & r < 225)
-						{
-							hero->SetLeft(true);
-							hero->SetX(x - 64.0f+(m_scroll));
-							hero->SetVX(-hero->GetVX()*0.1f);
-						}
-
-						//下
-						if (r > 225 && r < 315)
-						{
-							hero->SetUp(true);
-							hero->SetY(y + 64.0f);
-							if (hero->GetVY() < 0)
-							{
-								hero->SetVY(0.0f);
-							}
-						}
-				    }
-					
-					
-				}
-			}
-		}
-	}
-
-	
 }
 //ドロー
 void CObjBlock::Draw()
@@ -153,8 +69,8 @@ void CObjBlock::Draw()
 			if (m_map[i][j] > 0)
 			{
 				//表示位置の設定
-				dst.m_top = i *64.0f-64.0f;
-				dst.m_left = j *64.0f+m_scroll;
+				dst.m_top = i * 64.0f-64.0f;
+				dst.m_left = j * 64.0f + m_scroll;
 				dst.m_right = dst.m_left + 64.0f;
 				dst.m_bottom = dst.m_top + 64.0f;
 
@@ -164,4 +80,129 @@ void CObjBlock::Draw()
 		}
 	}
 	
+}
+
+//BlockDrawMethod関数
+//引数1 float x  :リソースの切り取り位置X
+//引数2 float y  :リソースの切り取り位置Y
+//引数3 RECT_F* dst  :描画位置
+//引数4 float c[]  :カラー情報
+//ブロックを64×64限定描画用。リソース切り取り位置のみx,yで設定できる
+void CObjBlock::BlockDraw(float x, float y, RECT_F *dst, float c[])
+{
+	RECT_F src;
+	src.m_top = y;
+	src.m_left = x;
+	src.m_right = src.m_left + 64.0f;
+	src.m_bottom = src.m_top + 64.0f;
+	//描画
+	Draw::Draw(0, &src, dst, c, 0.0f);
+}
+
+//BlockHit関数
+//引数1 float*x  :判定を行うobjectのX位置
+//引数2 float*y  :判定を行うobjectのY位置
+//引数3 bool scroll_on  :判定を行うobjectはスクロールの影響を与えるかどうか(true=与える　false=与えない)
+//引数4 bool* up  :上下左右判定の上部分にあたっているかどうかを返す
+//引数5 bool*　down  :上下左右判定の下部分にあたっているかどうかを返す
+//引数6 bool* left  :上下左右判定の左部分にあたっているかどうかを返す
+//引数7 bool* right  :上下左右判定の右部分にあたっているかどうかを返す
+//引数8 float* vx  :左右判定時の反発による移動方向・力の値に変えて返す
+//引数9 float* vy  :上下判定時による自由落下運動の移動方向・力の値に変えて返す
+//引数10 int* bt  :下部分判定時特殊なブロックのタイプを返す
+//判定を行うobjectとブロック64×64限定で、当たり判定と上下左右判定を行う
+//その結果は引数4～10に返す
+void CObjBlock::BlockHit(float *x, float *y, bool scroll_on, bool *up, bool *down,
+	bool *left, bool *right, float *vx, float *vy)
+{
+
+	*up = false;
+	*down = false;
+	*left = false;
+	*right = false;
+
+
+
+	for (int i = 0; i < 11; i++)
+	{
+		for (int j = 0; j < 157; j++)
+		{
+			//m_mapの全要素にアクセス
+			if (m_map[i][j] > 0)
+			{
+				//要素番号を座標に変更
+				float bx = j * 64.0f;
+				float by = i * 64.0f - 64.0f;
+
+				//スクロールの影響
+				float scroll = scroll_on ? m_scroll : 0;
+
+				//オブジェクトとブロックの当たり判定
+				if ((*x + (-m_scroll) + 64.0f > bx) && (*x + (-m_scroll) < bx + 64.0f) && (*y + 64.0f > by) && (*y < by + 64.0f))
+				{
+					//上下左右判定
+
+					//vectorの作成
+					float rvx = (*x + (-m_scroll)) - bx;
+					float rvy = *y - by;
+
+					//長さを求める
+					float len = sqrt(rvx * rvx + rvy * rvy);
+
+					//角度を求める
+					float r = atan2(rvy, rvx);
+					r = r * 180.0f / 3.14f;
+
+					if (r <= 0.0f)
+						r = abs(r);
+					else
+						r = 360.0f - abs(r);
+					//lenがある一定の長さより短い場合判定に入る
+					if (len < 88.0f)
+					{
+						//角度で上下左右を判定
+					//右
+						if ((r < 45 && r > 0) || r > 315)
+						{
+							*right=true;
+							*x = bx + 64.0f + (scroll);
+							*vx = -(*vx)*0.1f;
+						}
+
+						//上
+						if (r > 45 && r < 135)
+						{
+							*down = true;
+							*y = by - 64.0f;
+						
+						
+							*vy = 0.0f;
+						}
+
+						//左
+						if (r > 135 & r < 225)
+						{
+							*left = true;
+							*x = bx - 64.0f + (scroll);
+							*vx = -(*vx)*0.1f;
+						}
+
+						//下
+						if (r > 225 && r < 315)
+						{
+							*up = true;
+							*y = by + 64.0f;
+							if (*vy < 0)
+							{
+								*vy = 0.0f;
+							}
+						}
+					}
+
+
+				}
+			}
+		}
+	}
+
 }
--- BDゲーム/BDゲーム/WolkEnemy.h
+++ BDゲーム/BDゲーム/WolkEnemy.h
@@ -15,18 +15,21 @@ public:
 	void Action();
 	void Draw();
 
-	float GetX() { return m_px; }
-	float GetY() { return m_py; }
 private:
 	float m_px;
 	float m_py;
 	float m_vx;
 	float m_vy;
 	float m_posture;
-	bool m_hit_down;
-	float x;
+	
 	int m_ani_time;
 	int m_ani_frame;
 
+	float m_speed_power;
+	float m_ani_max_time;
 
+	bool m_hit_up;
+	bool m_hit_down;
+	bool m_hit_left;
+	bool m_hit_right;
 };
