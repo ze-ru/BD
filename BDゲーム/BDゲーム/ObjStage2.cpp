453fb0c1254409de11276b68ad7b9c98f4978864
岩本紫苑 <h1201916004@hiratagakuen.onmicrosoft.com>
2019-10-29 16:08:35 +0900

    u

---
+++ BDゲーム/BDゲーム/ObjStage2.cpp
@@ -0,0 +1,38 @@
+#include "GameL\DrawTexture.h"
+#include"GameL\WinInputs.h"
+#include"GameL\SceneManager.h"
+#include"GameL\DrawFont.h"
+#include"ObjStage2.h"
+
+using namespace GameL;
+
+void CObjStage2::Init()
+{
+
+}
+
+void CObjStage2::Action()
+{
+
+}
+
+void CObjStage2::Draw()
+{
+	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
+
+	RECT_F src;
+	RECT_F dst;
+
+	src.m_top = 0.0f;
+	src.m_left = 0.0f;
+	src.m_right = 800.0f;
+	src.m_bottom = 600.0f;
+
+	dst.m_top = 0.0f;
+	dst.m_left = 0.0f;
+	dst.m_right = 800.0f;
+	dst.m_bottom = 600.0f;
+
+	Draw::Draw(2, &src, &dst, c, 0.0f);
+}
+
--- BDゲーム/BDゲーム/ObjStage2.h
+++ BDゲーム/BDゲーム/ObjStage2.h
@@ -1,1 +1,18 @@
 #pragma once
+//使用ヘッダー
+#include"GameL\SceneObjManager.h"
+
+//使用ネームスペース
+using namespace GameL;
+
+//シーン:メイン
+class CObjStage2 :public CObj
+{
+public:
+	CObjStage2() {};
+	~CObjStage2() {};
+	void Init();
+	void Action();
+	void Draw();
+private:
+};
