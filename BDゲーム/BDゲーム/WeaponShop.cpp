1aa949b5625f0b34ce5eadde2aadc6903f6bb2bf
松崎 <toyotomioda1590@docomo.ne.jp>
2019-11-12 14:42:11 +0900

    h

---
+++ BDゲーム/BDゲーム/WeaponShop.cpp
@@ -0,0 +1,24 @@
+#include "GameL\DrawTexture.h"
+#include "GameL\WinInputs.h"
+#include "GameL\SceneManager.h"
+#include"GameL\SceneObjManager.h"
+#include "GameHead.h"
+#include"WeaponShop.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+void SceneWeaponShop::Init()
+{
+	
+}
+
+void SceneWeaponShop::Action()
+{
+
+}
+
+void SceneWeaponShop::Draw()
+{
+	
+}
