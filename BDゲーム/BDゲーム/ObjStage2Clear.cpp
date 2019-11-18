e4a1d931a77ef4ba06810430bf9cce3fe5acf0da
松崎 <toyotomioda1590@docomo.ne.jp>
2019-11-18 11:53:34 +0900

    クリア

---
+++ BDゲーム/BDゲーム/ObjStage2Clear.cpp
@@ -0,0 +1,41 @@
+﻿#include "GameL\DrawTexture.h"
+#include "GameL\WinInputs.h"
+#include "GameHead.h"
+#include "ObjStage2Clear.h"
+#include"GameL\SceneObjManager.h"
+
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjStage2Clear::Init()
+{
+	
+	Sc2 = false;//キーフラグ
+
+
+}
+
+//アクション
+void CObjStage2Clear::Action()
+{
+	if ()
+	{
+		Sc2 = true;
+	}
+}
+
+//ドロー
+void CObjStage2Clear::Draw()
+{
+	float c[4] = { 1,1,1,1 };
+
+
+	
+	if (Sc2 == true)
+	{
+		Font::StrDraw(L"Game Clear", 85, 70, 180, c);
+	}
+	
+}
--- BDゲーム/BDゲーム/ObjStage2Clear.h
+++ BDゲーム/BDゲーム/ObjStage2Clear.h
@@ -1,1 +1,23 @@
 #pragma once
+//使用ヘッダー
+#include"GameL\SceneObjManager.h"
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト：タイトル
+class CObjStage2Clear :public CObj
+{
+public:
+	CObjStage2Clear() {};
+	~CObjStage2Clear() {};
+	void Init();   //イニシャライズ
+	void Action(); //アクション
+	void Draw();   //ドロー
+	int Getmap1(int x, int y) { return map[x][y]; }
+	int Getmap2(int x, int y) { return map2[x][y]; }
+
+private:
+	int map[11][157];
+	int map2[11][156];
+	int Sc2;
+};
