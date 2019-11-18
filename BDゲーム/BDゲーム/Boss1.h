07a586d1994e117d235be541b27b751f28d59d52
岩本紫苑 <h1201916004@hiratagakuen.onmicrosoft.com>
2019-11-18 10:51:21 +0900

    変更ボス

--- BDゲーム/BDゲーム/Boss1.h
+++ BDゲーム/BDゲーム/Boss1.h
@@ -1,1 +1,26 @@
 #pragma once
+//
+#include"GameL\SceneObjManager.h"
+//
+using namespace GameL;
+
+//
+class CObjBoss1 :public CObj
+{
+public:
+	CObjBoss1(float x, float y);
+	~CObjBoss1() {};
+	void Init();
+	void Action();
+	void Draw();
+private:
+	float m_ex;
+	float m_ey;
+	float m_vx;
+	float m_vy;
+	
+	bool m_hit_up;
+	bool m_hit_down;
+	bool m_hit_left;
+	bool m_hit_right;
+};
