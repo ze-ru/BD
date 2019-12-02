13f513166d01f41b24780792fa034c3a77a64739
松崎 <toyotomioda1590@docomo.ne.jp>
2019-12-02 12:34:18 +0900

    ゲームオーバー画面

--- BDゲーム/BDゲーム/ObjGameOver.cpp
+++ BDゲーム/BDゲーム/ObjGameOver.cpp
@@ -28,11 +28,11 @@ void CObjGameOver::Action()
 {
 	if (Input::GetVKey(VK_UP) == true)
 	{
-		key = false;
+		key = true;
 	}
 	if (Input::GetVKey(VK_DOWN) == true)
 	{
-		key = true;
+		key = false;
 	}
 
 
@@ -69,12 +69,12 @@ void CObjGameOver::Draw()
 	Font::StrDraw(L"YOU LOST", 240, 70, 80, c);
 	Font::StrDraw(L"GAME OVER", 125, 200, 120, c);
 
-	if (key == false) 
+	if (key == true) 
 	{
 		Font::StrDraw(L"▶つづきから", 155, 380, 60, c);
 		Font::StrDraw(L" タイトルに戻る", 185, 480, 60, c);
 	}
-	if (key == true)
+	if (key == false)
 	{
 		Font::StrDraw(L" つづきから", 185, 380, 60, c);
 		Font::StrDraw(L"▶タイトルに戻る", 155, 480, 60, c);
